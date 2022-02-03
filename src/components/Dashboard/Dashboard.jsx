5ad721e0cf0d6c9b18a1e894c277fdf1c25db926
Patryk Jaworski <pjaworski.dev@gmail.com>
2022-02-03 20:36:34 +0100

    Add aside body prop

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,13 +2,23 @@ import React from 'react';
 import AsideBlock from './Components/Aside/Aside';
 import s from './Dashboard.module.css';
 
+
 const Dashboard = (props) => {
     return (
         <div className={s.dashboard_overlay}>
             <AsideBlock />
-            <header className={s.header_dash}></header>
-            <main className={s.main_dash}></main>
-            <footer className={s.footer_dash}></footer>
+            <div className={s.dashboard_content}>
+                <header className={s.header_dash}>1123</header>
+                <main className={s.main_dash}></main>
+                <footer className={s.footer_dash}></footer>
+            </div>
+            {/* inline styles for body-fix */}
+            <style>{"\
+        body{\
+          overflow:hidden;\
+          height:100vh;\
+        }\
+      "}</style>
         </div>
     )
 };
