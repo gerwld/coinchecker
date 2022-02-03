cdc11730b1fd503fdb2aa3d25253850eb922c7e2
Patryk Jaworski <pjaworski.dev@gmail.com>
2022-02-03 22:13:53 +0100

    Minor changes

--- src/components/Dashboard/Components/Aside/Aside.jsx
+++ src/components/Dashboard/Components/Aside/Aside.jsx
@@ -5,20 +5,21 @@ import { RiMenuFoldLine } from 'react-icons/ri';
 import { MdOutlineSpaceDashboard } from 'react-icons/md';
 
 const AsideBlock = (props) => {
-
+    //Aside toggle
     const [isAsideHide, setAside] = useState(false);
     const asideBtn = () => {
         setAside(!isAsideHide);
-      };
+    };
+    const logoText = isAsideHide ? 'CC' : 'CoinChecker';
 
     return (
         <aside className={`${s.aside_overlay} ${isAsideHide ? s.aside_hidden : ''}`}>
-            <div className={s.logo}>{isAsideHide ? 'CC' : 'CoinChecker'}</div>
-            <button onClick={asideBtn} className={s.toggleAside}><RiMenuFoldLine/></button>
+            <div className={s.logo}>{logoText}</div>
+            <button onClick={asideBtn} className={s.toggleAside}><RiMenuFoldLine /></button>
 
             <nav className={s.aside_menu}>
                 <ul>
-                    <li><NavLink exact to="/dashboard" className={(e) => e.isActive ? s.active_a : ''}><MdOutlineSpaceDashboard/>Dashboard</NavLink></li>
+                    <li><NavLink exact to="/dashboard" className={(e) => e.isActive ? s.active_a : ''}><MdOutlineSpaceDashboard />Dashboard</NavLink></li>
                     <li><NavLink to="/Inventory" className={s.dropdown}>Inventory</NavLink></li>
                     <li><NavLink to="/" className={s.dropdown}>CRM</NavLink></li>
                     <li><NavLink to="/">Orders</NavLink></li>
