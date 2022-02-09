36c53a581d5e51b7329c35114cd1266645bb85dd
P <>
2022-02-09 11:21:57 +0100

    Add chart

--- src/components/MainScreen/Header/HeaderTrends/HeaderTrends.jsx
+++ src/components/MainScreen/Header/HeaderTrends/HeaderTrends.jsx
@@ -14,7 +14,7 @@ const HeaderTrends = ({ headTrends }) => {
                     let price = Dinero({ amount: parseInt(e.price * 100), currency: 'USD' }).toFormat('$0,0.00');
                     return (<div key={e.id + e.name} className={s.sect2_block}>
                         <div className={s.sect2_block_subblock}>
-                            <div className={s.icon}><ShowImage url={e.logoUrl} alt='{e.alt}' newUrl="img/coin.svg" /></div>
+                            <div className={s.icon}><ShowImage url={e.logoUrl} alt={e.ticker} newUrl="img/coin.svg" /></div>
                             <div className={s.name}>
                                 <span className={s.ticker}>{e.ticker}</span>
                                 <span className={s.fullname}>{e.name}</span>
@@ -26,12 +26,12 @@ const HeaderTrends = ({ headTrends }) => {
                                 <span className={s.price}>{price}</span>
                                 <span className={s.percent}>2.14%</span>
                             </div>
-                            <div className={s.chart}>Chart</div>
+                            <div className={s.chart}><img src="/img/example-chart.svg" alt="Chart" /></div>
                         </div>
                     </div>)
                 })}
             </div>
-            
+
         </div>
     )
 }
