c94ff48bce97f5dbdac9b08b454dcde68cf6a0d3
P Jaworski <>
2022-02-09 15:08:42 +0100

    Add eslint, change tab to 2 spaces

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,65 +1,49 @@
-import React from 'react';
-import { Field, reduxForm } from 'redux-form';
-import s from './Login.module.css';
-import { NavLink } from 'react-router-dom';
-import LoginGithub from 'react-login-github';
+import React from "react";
+import { Field, reduxForm } from "redux-form";
+import s from "./Login.module.css";
+import { NavLink } from "react-router-dom";
+import LoginGithub from "react-login-github";
 
 const Login = (props) => {
-    return (
-        <div className={s.login_content}>
-
-            <div className={s.sect_overlay}>
-                <h1 className={s.title}>Sign in to CoinChecker</h1>
-                <div className={s.login_block}>
-                    <div className={s.overlay}>
-                        <LoginFormRedux onSubmit={props.loginAction} />
-                        <LoginGithub className={s.btn_github} clientId="826daa3627e56e610860"
-                            onSuccess={props.sendGitCodeToServ}
-                            onFailure={() => alert('Some error occured.Please, try another method.')} />
-                    </div>
-                    <div className={s.more_links}>
-                        <NavLink to='/register'>Sign Up</NavLink>
-                        <NavLink to='forgot'>Forgot password?</NavLink>
-                    </div>
-                </div>
-            </div>
-
+  return (
+    <div className={s.login_content}>
+      <div className={s.sect_overlay}>
+        <h1 className={s.title}>Sign in to CoinChecker</h1>
+        <div className={s.login_block}>
+          <div className={s.overlay}>
+            <LoginFormRedux onSubmit={props.loginAction} />
+            <LoginGithub className={s.btn_github} clientId="826daa3627e56e610860" onSuccess={props.sendGitCodeToServ} onFailure={() => alert("Some error occured.Please, try another method.")} />
+          </div>
+          <div className={s.more_links}>
+            <NavLink to="/register">Sign Up</NavLink>
+            <NavLink to="forgot">Forgot password?</NavLink>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 const LoginForm = ({ handleSubmit }) => {
-    return (
-        <form onSubmit={handleSubmit}>
-            <div className={s.login_field}>
-                <label>Login:</label>
-                <Field
-                    name="log_login"
-                    component="input"
-                    type="text"
-                    autoComplete="email"
-                    placeholder="Enter your email"
-                    required={true}
-                />
-            </div>
-            <div className={s.login_field}>
-                <label>Password:</label>
-                <Field
-                    name="log_password"
-                    component="input"
-                    type="password"
-                    autoComplete="current-password"
-                    placeholder="Enter your password"
-                    required={true}
-                />
-            </div>
-            <div className={s.login_buttons}>
-                <button type="submit" className={s.btn_login}>Sign In</button>
-            </div>
-        </form>
-    )
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className={s.login_field}>
+        <label>Login:</label>
+        <Field name="log_login" component="input" type="text" autoComplete="email" placeholder="Enter your email" required={true} />
+      </div>
+      <div className={s.login_field}>
+        <label>Password:</label>
+        <Field name="log_password" component="input" type="password" autoComplete="current-password" placeholder="Enter your password" required={true} />
+      </div>
+      <div className={s.login_buttons}>
+        <button type="submit" className={s.btn_login}>
+          Sign In
+        </button>
+      </div>
+    </form>
+  );
+};
 
-const LoginFormRedux = reduxForm({ form: 'login' })(LoginForm);
+const LoginFormRedux = reduxForm({ form: "login" })(LoginForm);
 
 export default Login;
--- src/components/MainScreen/Header/Header.jsx
+++ src/components/MainScreen/Header/Header.jsx
@@ -1,74 +1,81 @@
-import React, { useRef, useState } from 'react';
+import React, {useRef, useState} from 'react';
 import s from './Header.module.css';
-import { NavLink } from 'react-router-dom';
-import { HashLink as Link } from 'react-router-hash-link';
+import {NavLink} from 'react-router-dom';
+import {HashLink as Link} from 'react-router-hash-link';
 import useOutsideClickHide from '../../../helpers/hideOutsideClick';
-import { Pl, Ua, Us } from 'react-flags-select';
+import {Pl, Ua, Us} from 'react-flags-select';
 import HeaderTrends from './HeaderTrends/HeaderTrends';
 
 const HeaderMain = (props) => {
-    return (
-        <>
-            <header className={s.main_header}>
-                <div className={`${s.header_overlay} content-wrapper`}>
-                    <span className={s.header_logo}>CoinChecker</span>
-                    <nav className={s.main_nav} role="navigation">
-                        <button className={s.mobile_menu}>☰ Menu</button>
-                        <div className={s.element}>
-                            <NavLink to="/" className={(e) => e.isActive ? 'active_a' : ''}><span>Main</span></NavLink>
-                        </div>
-                        <div className={s.element}>
-                            <NavLink to="/dashboard" className={(e) => e.isActive ? 'active_a' : ''}><span>Dashboard</span></NavLink>
-                        </div>
-                        <div className={s.element}>
-                            <NavLink to="/" className={(e) => e.isActive ? 'active_a' : ''}><span>Budget</span></NavLink>
-                        </div>
-                        <div className={s.element}>
-                            <NavLink to="/" className={(e) => e.isActive ? 'active_a' : ''}><span>Market</span></NavLink>
-                        </div>
-                        <div className={s.element}>
-                            <NavLink to="/" className={(e) => e.isActive ? 'active_a' : ''}><span>About</span></NavLink>
-                        </div>
-                    </nav>
-                    <nav className={s.main_nav} role="navigation">
-                        <LangSelect currLang='EN' />
-                        <div className={s.element_btn}>
-                            <NavLink to="/login" className={(e) => e.isActive ? 'active_a' : ''}><span>Sign In</span></NavLink>
-                        </div>
-                    </nav>
-                </div>
-            </header>
-            <div className={s.header_section1}>
-                <div className="content-wrapper">
-                    <h1 className={s.header_title}>Start and Build Your Crypto Portfolio Here</h1>
-                    <p className={s.header_subtitle}><span>Only at CoinChecker, you can build a good portfolio and learn</span><span>best practices about cryptocurrency.</span></p>
-                    <Link to="#about_us" className={s.explore}>Get started</Link>
-                </div>
-            </div>
-            <HeaderTrends headTrends={props.headTrends} />
-        </>
-    )
-}
+  return (
+    <>
+      <HeaderSection />
+      <div className={s.header_section1}>
+        <div className="content-wrapper">
+          <h1 className={s.header_title}>Start and Build Your Crypto Portfolio Here</h1>
+          <p className={s.header_subtitle}><span>Only at CoinChecker, you can build a good portfolio and learn</span><span>best practices about cryptocurrency.</span></p>
+          <Link to="#about_us" className={s.explore}>Get started</Link>
+        </div>
+      </div>
+      <HeaderTrends headTrends={props.headTrends} />
+    </>
+  );
+};
+
+const HeaderSection = (props) => {
+  const headBar = useRef(0);
+  const [isFixed, setFixed] = useState(false);
+
+  return (
+    <header ref={headBar} className={`${s.main_header} ${isFixed ? s.fixed : ''}`}>
+      <div className={`${s.header_overlay} content-wrapper`}>
+        <span className={s.header_logo}>CoinChecker</span>
+        <nav className={s.main_nav} role="navigation">
+          <button className={s.mobile_menu}>☰ Menu</button>
+          <div className={s.element}>
+            <NavLink to="/" className={(e) => e.isActive ? 'active_a' : ''}><span>Main</span></NavLink>
+          </div>
+          <div className={s.element}>
+            <NavLink to="/dashboard" className={(e) => e.isActive ? 'active_a' : ''}><span>Dashboard</span></NavLink>
+          </div>
+          <div className={s.element}>
+            <NavLink to="/" className={(e) => e.isActive ? 'active_a' : ''}><span>Budget</span></NavLink>
+          </div>
+          <div className={s.element}>
+            <NavLink to="/" className={(e) => e.isActive ? 'active_a' : ''}><span>Market</span></NavLink>
+          </div>
+          <div className={s.element}>
+            <NavLink to="/" className={(e) => e.isActive ? 'active_a' : ''}><span>About</span></NavLink>
+          </div>
+        </nav>
+        <nav className={s.main_nav} role="navigation">
+          <LangSelect currLang='EN' />
+          <div className={s.element_btn}>
+            <NavLink to="/login" className={(e) => e.isActive ? 'active_a' : ''}><span>Sign In</span></NavLink>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 const LangSelect = (props) => {
-    //hide lang
-    const [isShow, set] = useState(false);
-    const e = useRef(null);
-    useOutsideClickHide(e, set, null);
+  // hide lang
+  const [isShow, set] = useState(false);
+  const e = useRef(null);
+  useOutsideClickHide(e, set, null);
 
-    return (
-        <div ref={e} className={`${s.element_lang} ${isShow ? s.show_block : ''}`}>
-            <button onClick={() => set(!isShow)} className={`${s.btn_lang} ic-dropdown`}>EN</button>
-            <div className={s.lang_drop}>
-                <div className={s.lang_drop_option}><Us /> English</div>
-                <div className={s.lang_drop_option}><Ua /> Ukraininan</div>
-                <div className={s.lang_drop_option}><Pl /> Polish</div>
-            </div>
-        </div>
-    )
-}
-
-
+  return (
+    <div ref={e} className={`${s.element_lang} ${isShow ? s.show_block : ''}`}>
+      <button onClick={() => set(!isShow)} className={`${s.btn_lang} ic-dropdown`}>EN</button>
+      <div className={s.lang_drop}>
+        <div className={s.lang_drop_option}><Us /> English</div>
+        <div className={s.lang_drop_option}><Ua /> Ukraininan</div>
+        <div className={s.lang_drop_option}><Pl /> Polish</div>
+      </div>
+    </div>
+  );
+};
 
 
 export default HeaderMain;
