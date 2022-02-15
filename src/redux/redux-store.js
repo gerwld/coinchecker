7920bc53a4b60441981bc98891f29208048ba62d
P Jaworski <>
2022-02-15 12:21:26 +0100

    Remove devtools for prod

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,5 +1,4 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
 import { reducer as formReducer } from 'redux-form';
 import thunk from 'redux-thunk';
 import appReducer from "./app-reducer";
@@ -13,8 +12,8 @@ let reducers = combineReducers({
     form: formReducer
 });
 
-const store = createStore(reducers, composeWithDevTools(
+const store = createStore(reducers, 
     applyMiddleware(thunk)
-));
+);
 
 export default store;
