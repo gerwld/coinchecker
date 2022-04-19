531427c7bffb25fef16c9c28f9a8dbe4440aa25d
Patryk Jaworski <pjaworski.dev@gmail.com>
2022-04-19 20:11:43 +0200

    Add trans popup style

--- src/components/pages/Dashboard/blocks/ShowCoinsBlock/TransactionPopup/TransactionPopup.jsx
+++ src/components/pages/Dashboard/blocks/ShowCoinsBlock/TransactionPopup/TransactionPopup.jsx
@@ -1,82 +1,103 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import { Form, Field } from 'react-final-form'
+import { Form, Field } from "react-final-form";
 import s from "./Trans.module.css";
 import { useDispatch } from "react-redux";
 import WalletService from "../../../../../../api/WalletService";
 import { closeTransPopup } from "../../../../../../redux/reducers/dashboard-reducer";
 import { getAllWalletsTC } from "../../../../../../redux/reducers/wallets-reducer";
+import { IoCloseOutline } from "react-icons/io5";
 
-const TransactionPopup = ({  }) => {
- const dispatch = useDispatch();
- const [transType, setTransType] = useState(0);
- const {item, isShow} = useSelector(({dashboard}) => ({
-  item: dashboard.transCoin,
-  isShow: dashboard.isTransPopup
- }))
- const setClose = () => {
-  dispatch(closeTransPopup);
- }
+const TransactionPopup = ({}) => {
+  const dispatch = useDispatch();
+  const [transType, setTransType] = useState(0);
+  const { item, isShow } = useSelector(({ dashboard }) => ({
+    item: dashboard.transCoin,
+    isShow: dashboard.isTransPopup,
+  }));
+  const buttons = [
+    { id: 0, title: "Buy" },
+    { id: 1, title: "Sell" },
+    { id: 2, title: "Transfer" },
+  ];
+  const setClose = () => {
+    dispatch(closeTransPopup);
+  };
 
- const onSubmit = async (e) => {
-  const data = {
-   amount: e.amount,
-   datetime: e.datetime,
-   price: e.price,
-   coinId: item.id
-  }
-  await WalletService.buyCoinInWalletId(53, data);
-  dispatch(getAllWalletsTC());
-  setClose();
-  console.log(data);
- }
- 
- if(isShow) return (
-    <div className={s.popup_wrapper}>
-      <div className={s.popup_content}>
-      <button onClick={setClose} className={s.btn_close}>close</button>
-      <div className="transaction_type">
-       <button>Buy</button>
-       <button>Sell</button>
-       <button>Transfer</button>
+  const onSubmit = async (e) => {
+    const data = {
+      amount: e.amount,
+      datetime: e.datetime,
+      price: e.price,
+      coinId: item.id,
+    };
+    await WalletService.buyCoinInWalletId(53, data);
+    dispatch(getAllWalletsTC());
+    setClose();
+    console.log(data);
+  };
+
+  if (isShow)
+    return (
+      <div className={s.popup_wrapper}>
+        <div className={s.popup_content}>
+          <button onClick={setClose} className={s.btn_close}>
+            <IoCloseOutline />
+          </button>
+          <h2>Add Transaction to My Portfolio</h2>
+          <div className={s.trans_buttons}>
+            {buttons.map((e) => (
+              <button onClick={() => setTransType(e.id)} className={e.id === transType && s.active}>
+                {e.title}
+              </button>
+            ))}
+          </div>
+          <Form
+            onSubmit={onSubmit}
+            render={({ handleSubmit }) => (
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <label>
+                    Price per coin<span className={s.red}>*</span>
+                  </label>
+                  <Field name="price" component="input" type="number" placeholder="USD" required />
+                </div>
+                <div>
+                  <label>
+                    Quantity<span className={s.red}>*</span>
+                  </label>
+                  <div className={s.amount_input}>
+                    <Field name="amount" component="input" type="number" placeholder="1" required />
+                    <span>{item.symbol}</span>
+                  </div>
+                </div>
+                <div>
+                  <div className={s.total_opt}>
+                    <label>Total Spent</label>
+                    <button type="button">Price per coin</button>
+                  </div>
+                  <div className={s.total_field}>
+                    <Field name="usdAmount" component="input" type="number" placeholder="0" disabled />
+                  </div>
+                </div>
+                <div>
+                  <label>
+                    Date<span className={s.red}>*</span>
+                  </label>
+                  <Field name="datetime" component="input" type="date" placeholder="Date" required />
+                </div>
+                Fee & Notes (Opt)
+                <div className={s.action_popup}>
+                  <button type="button">Cancel</button>
+                  <button type="submit">Submit</button>
+                </div>
+              </form>
+            )}
+          />
+        </div>
+        <div className={s.popup_bg} onClick={setClose} />
       </div>
-      <Form
-    onSubmit={onSubmit}
-    render={({ handleSubmit }) => (
-      <form onSubmit={handleSubmit}>
-        <h2>Add Transaction to My Portfolio</h2>
-
-        <div>
-          <label>Price per coin</label>
-          <Field name="price" component="input" placeholder="USD" required />
-        </div>
-
-        <div>
-          <label>Quantity</label>
-          <Field name="amount" component="input" placeholder="1" required/>
-          <span>{item.symbol}</span>
-        </div>
-
-        <div>
-          <label>Total Spent</label>
-          <Field name="usdAmount" component="input" placeholder="0" disabled/>
-        </div>
-
-        <div>
-          <label>Date</label>
-          <Field name="datetime" component="input" type="date" placeholder="Date" required />
-        </div>
-
-        Fee & Notes (Opt)
-
-        <button type="submit">Submit</button>
-      </form>
-    )}
-  />
-      </div>
-      <div className={s.popup_bg} onClick={setClose}/>
-    </div>
-  );
+    );
   return <></>;
 };
 
