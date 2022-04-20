f26af867224f6747d20dad0e761322cba98684c1
Patryk Jaworski <pjaworski.dev@gmail.com>
2022-04-20 23:09:34 +0200

    Add sell

--- src/components/pages/Dashboard/blocks/ShowCoinsBlock/TransactionPopup/TransactionPopup.jsx
+++ src/components/pages/Dashboard/blocks/ShowCoinsBlock/TransactionPopup/TransactionPopup.jsx
@@ -23,21 +23,22 @@ const TransactionPopup = () => {
     item: dashboard.transCoin,
     isShow: dashboard.isTransPopup,
     walletId: dashboard.walletId,
-    wallets: wallets.content
+    wallets: wallets.content,
   }));
 
   const getTitle = (walletId) => {
-    const walletObjId = wallets.find(wall => wall.id === walletId);
+    const walletObjId = wallets.find((wall) => wall.id === walletId);
     return walletObjId.name;
-  }
+  };
 
   const setClose = () => {
     dispatch(closeTransPopup);
+    setTransType(0);
   };
 
   const onSubmit = async (e) => {
     const data = {
-      amount: parseInt(e.amount),
+      amount: transType === 1 ? (parseInt(e.amount) * -1) : parseInt(e.amount),
       datetime: e.datetime,
       price: e.price,
       coinId: item.id,
@@ -49,92 +50,147 @@ const TransactionPopup = () => {
   };
 
   useEffect(() => {
-    if(!wallets && isShow) {
+    if (!wallets && isShow) {
       dispatch(getAllWalletsTC());
     }
-  }, [wallets, isShow])
+  }, [wallets, isShow]);
 
   if (isShow)
     return (
       <div className={s.popup_wrapper}>
         <div className={s.popup_content}>
-        <button onClick={setClose} className={s.btn_close}>
+          <button onClick={setClose} className={s.btn_close}>
             <IoCloseOutline />
           </button>
-        {walletId || wallets
-         ? <div className={s.popup_info}>
-  
-          <h2>Add Transaction {walletId && 'to ' + getTitle(walletId)}</h2>
-          <div className={s.trans_buttons}>
-            {buttons.map((e, i) => (
-              <button key={i + "_transbtn"} onClick={() => setTransType(e.id)} className={e.id === transType ? s.active : ""}>
-                {e.title}
-              </button>
-            ))}
-          </div>
-          <Form
-            onSubmit={onSubmit}
-            initialValues={{
-             "price": item.currentPrice
-            }}
-            render={({ handleSubmit }) => (
-              <form onSubmit={handleSubmit}>
+          {walletId || wallets ? (
+            <div className={s.popup_info}>
+              <h2>Add Transaction {walletId && "to " + getTitle(walletId)}</h2>
+              <div className={s.trans_buttons}>
+                {buttons.map((e, i) => (
+                  <button key={i + "_transbtn"} onClick={() => setTransType(e.id)} className={e.id === transType ? s.active : ""}>
+                    {e.title}
+                  </button>
+                ))}
+              </div>
+              <Form
+                onSubmit={onSubmit}
+                initialValues={{
+                  price: item.currentPrice,
+                }}
+                render={({ handleSubmit }) => (
+                  <form onSubmit={handleSubmit}>
+                    {!walletId && (
+                      <div>
+                        <label>
+                          Select Wallet:<span className={s.red}>*</span>
+                        </label>
+                        <Field name="walletId" component="select" defaultValue={wallets[0]?.id} required>
+                          {wallets.map((e, i) => (
+                            <option key={i + "_selectwalletpopup"} value={e.id}>
+                              {e.name}
+                            </option>
+                          ))}
+                        </Field>
+                      </div>
+                    )}
 
-                {!walletId && 
-                <div>
-                  <label>
-                    Select Wallet:<span className={s.red}>*</span>
-                  </label>
-                  <Field name="walletId" component="select" defaultValue={wallets[0]?.id} required>
-                    {wallets.map((e, i) => <option key={i + "_selectwalletpopup"} value={e.id}>{e.name}</option>)}
-                  </Field>
-                </div>}
+                    {transType === 0 && <BuyTransaction symbol={item.symbol} />}
+                    {transType === 1 && <SellTransaction symbol={item.symbol} />}
 
-                <div>
-                  <label>
-                    Price per coin<span className={s.red}>*</span>
-                  </label>
-                  <Field name="price" component="input" type="number" placeholder="USD" required />
-                </div>
-                <div>
-                  <label>
-                    Quantity<span className={s.red}>*</span>
-                  </label>
-                  <div className={s.amount_input}>
-                    <Field name="amount" component="input" type="number" placeholder="1" required />
-                    <span>{item.symbol}</span>
-                  </div>
-                </div>
-                <div>
-                  <div className={s.total_opt}>
-                    <label>Total Spent</label>
-                    <button type="button">Price per coin</button>
-                  </div>
-                  <div className={s.total_field}>
-                    <Field name="usdAmount" component="input" type="number" placeholder="0" disabled />
-                  </div>
-                </div>
-                <div>
-                  <label>
-                    Date<span className={s.red}>*</span>
-                  </label>
-                  <Field name="datetime" component="input" type="date" placeholder="Date" />
-                </div>
-                Fee & Notes (Opt)
-                <div className={s.action_popup}>
-                  <button onClick={setClose} type="button">Cancel</button>
-                  <button type="submit">Submit</button>
-                </div>
-              </form>
-            )}
-          />
-        </div>
-        : <EmbeddedLoader/>}
+                    <div className={s.action_popup}>
+                      <button onClick={setClose} type="button">
+                        Cancel
+                      </button>
+                      <button type="submit">Submit</button>
+                    </div>
+                  </form>
+                )}
+              />
+            </div>
+          ) : (
+            <EmbeddedLoader />
+          )}
         </div>
         <div className={s.popup_bg} onClick={setClose} />
       </div>
     );
   return <></>;
+};
+
+const BuyTransaction = ({ symbol }) => {
+  return (
+    <>
+      <div>
+        <label>
+          Price per coin<span className={s.red}>*</span>
+        </label>
+        <Field name="price" component="input" type="number" placeholder="USD" required />
+      </div>
+      <div>
+        <label>
+          Quantity<span className={s.red}>*</span>
+        </label>
+        <div className={s.amount_input}>
+          <Field name="amount" component="input" type="number" placeholder="1" required />
+          <span>{symbol}</span>
+        </div>
+      </div>
+      <div>
+        <div className={s.total_opt}>
+          <label>Total Spent</label>
+          <button type="button">Price per coin</button>
+        </div>
+        <div className={s.total_field}>
+          <Field name="usdAmount" component="input" type="number" placeholder="0" disabled />
+        </div>
+      </div>
+      <div>
+        <label>
+          Date<span className={s.red}>*</span>
+        </label>
+        <Field name="datetime" component="input" type="date" placeholder="Date" />
+      </div>
+      Fee & Notes (Opt)
+    </>
+  );
+};
+
+const SellTransaction = ({ symbol }) => {
+  return (
+    <>
+      <div>
+        <label>
+          Price per coin<span className={s.red}>*</span>
+        </label>
+        <Field name="price" component="input" type="number" placeholder="USD" required />
+      </div>
+      <div>
+        <label>
+          Quantity<span className={s.red}>*</span>
+        </label>
+        <div className={s.amount_input}>
+          <Field name="amount" component="input" type="number" placeholder="1" required />
+          <span>{symbol}</span>
+        </div>
+      </div>
+      <div>
+        <div className={s.total_opt}>
+          <label>Total Recieved</label>
+          <button type="button">Price per coin</button>
+        </div>
+        <div className={s.total_field}>
+          <Field name="usdAmount" component="input" type="number" placeholder="0" disabled />
+        </div>
+      </div>
+      <div>
+        <label>
+          Date<span className={s.red}>*</span>
+        </label>
+        <Field name="datetime" component="input" type="date" placeholder="Date" />
+      </div>
+      Fee & Notes (Opt)
+    </>
+  );
 };
 
 export default TransactionPopup;
