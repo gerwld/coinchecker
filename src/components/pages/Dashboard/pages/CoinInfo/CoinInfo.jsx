435601d687c8e4a6eb377553c63173a0acfb53bd
Patryk Jaworski <pjaworski.dev@gmail.com>
2022-04-22 18:01:32 +0200

    Add share btns

--- src/components/pages/Dashboard/pages/CoinInfo/CoinInfo.jsx
+++ src/components/pages/Dashboard/pages/CoinInfo/CoinInfo.jsx
@@ -1,60 +1,73 @@
-import React, { useEffect } from 'react'
-import {NavLink, useParams} from "react-router-dom";
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from "react";
+import { NavLink, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 import s from "./Coin.module.css";
-import { RiArrowRightSLine } from 'react-icons/ri';
-import { MdArrowDropDown } from 'react-icons/md';
+import { RiArrowRightSLine } from "react-icons/ri";
+import { MdArrowDropDown } from "react-icons/md";
+import { AiOutlineStar, AiOutlineBell, AiOutlineShareAlt } from "react-icons/ai";
 
-import { getPageCoinTC, resPageCoin } from '../../../../../redux/reducers/dashboard-reducer';
-import EmbeddedLoader from '../../../../UI/EmbeddedLoader/EmbeddedLoader';
-import ErrorScreen from '../../../../UI/ErrorScreen/ErrorScreen';
+import { getPageCoinTC, resPageCoin } from "../../../../../redux/reducers/dashboard-reducer";
+import EmbeddedLoader from "../../../../UI/EmbeddedLoader/EmbeddedLoader";
+import ErrorScreen from "../../../../UI/ErrorScreen/ErrorScreen";
 
 const CoinInfo = () => {
   const currentId = useParams().coinId;
   const dispatch = useDispatch();
-  const {data, error} = useSelector(({dashboard}) => ({
+  const { data, error } = useSelector(({ dashboard }) => ({
     data: dashboard.pageCoinData,
-    error: dashboard.pageCoinErr
-  }))
+    error: dashboard.pageCoinErr,
+  }));
   const perc = data?.marketCapChangePercentage24h ? Math.abs(data.marketCapChangePercentage24h).toFixed(1) : 0;
   const isMoreOrEq0 = data?.marketCapChangePercentage24h ? data?.marketCapChangePercentage24h >= 0 : false;
 
   useEffect(() => {
     dispatch(getPageCoinTC(currentId));
-    return() => dispatch(resPageCoin);
-  }, [currentId])
- 
-  if(data) return (
-    <div className={s.content_block}>
-      <div className={s.breadcrumbs}>
-        <NavLink to="/dashboard">Coins</NavLink>
-        <RiArrowRightSLine/>
-        <span>{data.name}</span>
-      </div>
-      <div className={s.coin_maininfo}>
-      <span className={s.rank}>Rank #{data.id}</span>
-      <div className={s.maininfo_block}>
-        <div className={s.icon}><img src={data.image} alt={data.name} /></div>
-        <span className={s.name}>{data.name} <span>({data.symbol})</span></span>
-      </div>
-      </div>
-      <div className={`${s.maininfo_block} ${s.price_block}`}>
-      <span className={s.price}>${data.currentPrice}</span>
-      <span className={`${s.price_dash} ${isMoreOrEq0 && s.priceup}`}><MdArrowDropDown />{perc}%</span>
-      </div>
-      USDprice (+-)
-      ShareBtns
+    return () => dispatch(resPageCoin);
+  }, [currentId]);
 
-      coininfo
-
-      Info
-
-      coin to
-
+  if (data)
+    return (
+      <div>
+        <div className={s.breadcrumbs}>
+          <NavLink to="/dashboard">Coins</NavLink>
+          <RiArrowRightSLine />
+          <span>{data.name}</span>
+        </div>
+        <div className={s.content_block}>
+          <div className={s.coin_maininfo}>
+            <span className={s.rank}>Rank #{data.id}</span>
+            <div className={s.maininfo_block}>
+              <div className={s.icon}>
+                <img src={data.image} alt={data.name} />
+              </div>
+              <span className={s.name}>
+                {data.name} <span>({data.symbol})</span>
+              </span>
+            </div>
+          </div>
+          <div className={`${s.maininfo_block} ${s.price_block}`}>
+            <span className={s.price}>${data.currentPrice}</span>
+            <span className={`${s.price_dash} ${isMoreOrEq0 && s.priceup}`}>
+              <MdArrowDropDown />
+              {perc}%
+            </span>
+          </div>
+          <div className={s.share_buttons}>
+            <button><AiOutlineShareAlt/></button>
+            <button><AiOutlineBell/></button>
+            <button><AiOutlineStar/></button>
+          </div>
+          USDprice (+-) ShareBtns coininfo Info coin to
+        </div>
+      </div>
+    );
+  else if (error) return <ErrorScreen error={error} withIcon />;
+  return (
+    <div className="dash_content_loader">
+      <EmbeddedLoader />
     </div>
-  ); else if(error) return <ErrorScreen error={error} withIcon/>;
-  return <div className="dash_content_loader"><EmbeddedLoader /></div>
-}
+  );
+};
 
 export default CoinInfo;
