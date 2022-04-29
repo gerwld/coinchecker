6147e850cb2ea638dd4e7b95c0cbb821ea943f5b
Patryk Jaworski <pjaworski.dev@gmail.com>
2022-04-29 18:16:29 +0200

    Fix embedded loader

--- src/components/UI/EmbeddedLoader/EmbeddedLoader.jsx
+++ src/components/UI/EmbeddedLoader/EmbeddedLoader.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import s from './Loader.module.css';
+import s from './EmbeddedLoader.module.css';
 
 function EmbeddedLoader() {
   return (
       <div className={s.preloader}>
-        <span className={s.preloader_ld}></span>
+        <div className={s.preloader_ld}>
+          <div className={s.loader}></div>
+        </div>
       </div>
   )
 }
