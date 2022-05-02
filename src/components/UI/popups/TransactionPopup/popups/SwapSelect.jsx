08fd71147cdfa4d33b53bc16d0bd481e37d9e8b5
Patryk Jaworski <pjaworski.dev@gmail.com>
2022-05-02 15:42:06 +0200

    Add items

--- src/components/UI/popups/TransactionPopup/popups/SwapSelect.jsx
+++ src/components/UI/popups/TransactionPopup/popups/SwapSelect.jsx
@@ -6,16 +6,50 @@ const SwapSelect = withClickOutside(({ refE, setShow, isShow, symbol, ignore }) 
   const onSetShow = () => setShow(!isShow);
   return (
     <div>
-      <button onClick={onSetShow} ref={ignore} className={isShow ? s.opened : ''} type="button">
+      <button onClick={onSetShow} ref={ignore} className={isShow ? s.opened : ""} type="button">
         {symbol}
         <span />
       </button>
-      {isShow 
-      ? <div className={s.select_block} ref={refE}>
+      {isShow ? (
+        <div className={s.select_block} ref={refE}>
           <div className={s.select_type}>
-           <input type="text" placeholder="Search..." />
-           я родился</div>
-        </div> : ''}
+            <input type="text" placeholder="Search..." />
+            <div className={s.select_options}>
+              <span>Cryptocurrencies</span>
+
+              <div className={s.content}>
+                <div className={s.elem}>
+                  <span>BTC</span>
+                  <span>Bitcoin</span>
+                </div>
+                <div className={s.elem}>
+                  <span>BTC</span>
+                  <span>Bitcoin</span>
+                </div>
+                <div className={s.elem}>
+                  <span>BTC</span>
+                  <span>Bitcoin</span>
+                </div>
+                <div className={s.elem}>
+                  <span>BTC</span>
+                  <span>Bitcoin</span>
+                </div>
+                <div className={s.elem}>
+                  <span>BTC</span>
+                  <span>Bitcoin</span>
+                </div>
+                <div className={s.elem}>
+                  <span>BTC</span>
+                  <span>Bitcoin</span>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 });
